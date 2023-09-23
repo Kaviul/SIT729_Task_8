e9ef651177a55214125a767352596181704efde6
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2023-09-23 18:01:14 +1000

    updates

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const client = mqtt.connect('mqtt://broker.hivemq.com:1883', {
     username : process.env.USERNAME,
     password : process.env.PASSWORD 
 });
-//const client = mqtt.connect('127.0.0.1:1883');
+
 
 const topic = "/sensor"
 
@@ -17,6 +17,6 @@ client.on("connect", function() {
          client.publish(topic, ' ' + random.toString()+".") 
     };
     
-}, 5000);
+}, 1000);
 })
 
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const client = mqtt.connect('mqtt://broker.hivemq.com:1883', {
     username : process.env.USERNAME,
     password : process.env.PASSWORD  
 });
-//const client = mqtt.connect('127.0.0.1:1883');
+
+
 const topic = "/sensor";
 
 client.on("connect", function () {
